13cb071c1ad8fc8cab739ab2b0b7d9de14e61735
Nam Hoai Duong <62597935+AtLastttttt@users.noreply.github.com>
2021-10-24 14:09:34 +0700

    Delete Task3.py

--- Task3.py
+++
@@ -1,9 +0,0 @@
-ngay = input("Ngay:")
-ten = input("Ten:")
-sang = int(input("So calo cho buoi sang:"))
-trua = int(input("So calo cho buoi trua:"))
-toi = int(input("So calo cho buoi toi:"))
-caloBurntToday = int(input("So calo da van dong trong ngay:"))
-total = sang + trua + toi - caloBurntToday
-a = "{} co so calo tich luy trong ngay {} la {}"
-print(a.format(ten, ngay, total))
