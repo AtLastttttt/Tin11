ec28f1206b116ad32200fcf73739340f0fad54b5
Nam Hoai Duong <62597935+AtLastttttt@users.noreply.github.com>
2021-10-24 14:41:59 +0700

    Add project order

--- b5.py
+++ b5.py
@@ -1,3 +1,14 @@
+'''
+Nhập ngày
+Nhập tên
+Nhập số calo cho buổi sáng
+Nhập số calo cho buổi trưa
+Nhập số calo cho buổi tối
+Nhập số calo đã vận động trong ngày
+Tính số calo tích lũy trong ngày = calo sáng + calo trưa + calo tối – calo vận động
+Xuất bạn tên có số calo tích lũy trong ngày là : số calo
+'''
+
 ngay = input("Ngay:")
 ten = input("Ten:")
 sang = int(input("So calo cho buoi sang:"))
