9e5d832419d239caa83eee65827b9caa1e42324d
Nam Hoai Duong <62597935+AtLastttttt@users.noreply.github.com>
2021-10-24 14:09:28 +0700

    Delete Task2.py

--- Task2.py
+++
@@ -1,9 +0,0 @@
-h = float(input())
-g = 9.8
-from math import sqrt
-v = sqrt(2*g*h)
-print("Van toc roi tu do cua vat theo cong:", round(v))h = float(input())
-g = 9.8
-from math import sqrt
-v = sqrt(2*g*h)
-print("Van toc roi tu do cua vat theo cong:", round(v))
